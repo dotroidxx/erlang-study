24922bdde6fbc6b697245e66b879e265fdd0b08d
Go Akahane <dotroid+github@gmail.com>
2011-03-28 17:53:31 +0900

    Change a workspase

--- hello.erl
+++ hello.erl
@@ -1,5 +1,24 @@
+%% Author: red
+%% Created: 2011/03/28
+%% Description: TODO: Add description to hello
 -module(hello).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
 -export([hello/0]).
 
+%%
+%% API Functions
+%%
+
+%%
+%% Local Functions
+%%
 hello() ->
 	io:fwrite("Hello, World! ~n",[]).
+
