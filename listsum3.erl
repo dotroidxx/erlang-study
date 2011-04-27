e74c5ec325beb4f46717a7b12e048a7b357c8357
Go Akahane <dotroid+github@gmail.com>
2011-04-27 11:34:11 +0900

    コメント追加

--- listsum3.erl
+++ listsum3.erl
@@ -6,7 +6,11 @@ sum(L) ->
 
 sum1([], Sum) ->
 	Sum;
+
 sum1([H | T], Sum) when is_integer(H) ->
+    %% ガードを使用して、Hが数値の場合のみ、この関数が呼び出される
 	sum1(T, Sum + H);
+
 sum1([_ | T], Sum) ->
+	%% 上記のsum1で、Hが数値以外の値の場合は、こちらが呼び出される
     sum1(T, Sum).
